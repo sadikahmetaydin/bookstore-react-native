d27d6430352062c4a934d6f5169d4c9e1da0bf8d
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-12-03 20:16:53 +0300

    create get books route and pagination

--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -6,11 +6,11 @@ import protectRoute from "../middleware/auth.middleware.js";
 const router = express.Router();
 
 // create a book
-router.post("/", protectRoute ,async (req, res) => {
+router.post("/", protectRoute, async (req, res) => {
   try {
     const { title, caption, rating, image } = req.body;
 
-    if (!image || !title || !caption || !rating ) {
+    if (!image || !title || !caption || !rating) {
       return res.status(400).json({ message: "Please provide all fields" });
     }
 
@@ -30,11 +30,38 @@ router.post("/", protectRoute ,async (req, res) => {
     await newBook.save();
 
     res.status(201).json(newBook);
-
   } catch (error) {
     console.log("Error creating book", error);
     res.status(500).json({ message: error.message });
   }
-})
+});
+
+// pagination => infinite loading
+router.get("/", protectRoute, async (req, res) => {
+  try {
+    const page = req.query.page || 1;
+    const limit = req.query.limit || 5;
+    const skip = (page - 1) * limit;
+
+    const books = await Book.find()
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit)
+      .populate("user", "username profileImage");
+
+    const totalBooks = await Book.countDocuments();
+
+    res.send({
+      books,
+      currentPage: page,
+      totalBooks,
+      totalPages: Math.ceil(totalBooks / limit),
+    });
+    
+  } catch (error) {
+    console.log("Error in get all books route", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
 
 export default router;
