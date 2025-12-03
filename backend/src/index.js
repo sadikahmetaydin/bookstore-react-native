ea3e4da4abd69f580bf1eeb72d2d60e5ad4b1eb1
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-12-03 20:48:29 +0300

    define cors package

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import "dotenv/config";
 
 import authRoutes from "./routes/authRoutes.js";
@@ -10,6 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/books", bookRoutes);
